171d50260e56e1ad3c67d9ac02220468332f246e
zhongjun_ge <shog86@gmail.com>
2025-07-17 12:12:17 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -53,12 +53,10 @@ const RICH_TITLE = true;
 const IS_CHINESE = true;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string =>
-  `户外运动 ${yearLength} 年 ` +
-  (year === 'Total' ? '' : `，地图展示的是 ${year} 年的轨迹`);
+  `这里记录了我过去 ${yearLength} 年的运动` + ( year === 'Total' ? '' : `，目前展示的是 ${year} 年的轨迹`);
 
 const ENGLISH_INFO_MESSAGE = (yearLength: number, year: string): string =>
-  `Logged ${yearLength} Years of Outdoor Journey` +
-  (year === 'Total' ? '' : `, the map show routes in ${year}`);
+  `Logged ${yearLength} years of my workouts` +  ( year === 'Total' ? '' : `, the map shows all my records in ${year}`);
 
 // not support English for now
 const CHINESE_LOCATION_INFO_MESSAGE_FIRST =
